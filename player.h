f840e792bde1045973f8ca57e34a91bd77ae4460
David Yang <dyang57@ucmerced.edu>
2022-05-07 22:45:08 -0700

    header functions made to estimate hitbox

--- player.h
+++ player.h
@@ -1,14 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <QPainter>
 #include "collidable.h"
 
 class Player : public Collidable
 {
 public:
-    Player(QGraphicsItem *parent);
+    Player();
 
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+               QWidget *widget) override;
+
+protected:
     void advance(int step) override;
+
+private:
+    qreal angle = 0;
+    qreal speed = 0;
+    QColor color;
 };
 
 #endif // PLAYER_H
