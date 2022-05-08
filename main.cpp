ce4188c9c2dd45827ed19978536906569ed69369
David Yang <dyang57@ucmerced.edu>
2022-05-07 21:35:45 -0700

    set minimum window dimensions

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Space Invaders");
+    QWidget widget;
+    widget.setMinimumSize(400, 300);
+    w.setCentralWidget(&widget);
     w.show();
     return a.exec();
 }
